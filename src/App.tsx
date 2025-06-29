b597c69cb77bc2b8a30612291145cc94b142a7d7
Sar-Kar666 <diptanusarkar699@gmail.com>
2025-06-29 18:08:23 +0530

    made a generic button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css'
+import { Button } from './components/Button'
+import { PlusIcon } from './icons/PlusIcon'
+import { ShareIcon } from './icons/ShareIcon'
 
 function App() {
   
 
   return (
-   <div className=' bg-red-50'>
-    hii there
-   </div>
+   <div >
+    <Button variant='primary' text="Add Content" startIcon={<PlusIcon/>}/>
+    <Button variant='secondary' text="Share Brain" startIcon={<ShareIcon/>}/>
+   </div> 
+ 
       
     
   )
